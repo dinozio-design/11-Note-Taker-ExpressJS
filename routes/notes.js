6c49781e23aa909edfb93d8bb211549c4606a30b
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-07-10 15:38:42 -0400

    persisting data as well as POST functional

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,20 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
-notes.get('/', (req, res) =>{
+notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = { title, text };
+        readAndAppend(newNote, './db/db.json');
+        res.json(`New Note Added Successfully!`);
+    } else {
+        res.error('Error in adding note');
+    }
 });
 
 module.exports = notes;
