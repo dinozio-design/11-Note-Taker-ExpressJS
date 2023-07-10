64087ce5dda743be25e17e58c5d1ff622d5ce91e
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-07-10 14:48:58 -0400

    api routs created and working it reads from the json file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
 // modular routers for api end points
+const notesRouter = require('./notes');
+const app = express();
+app.use ('/notes', notesRouter);
 
 
 module.exports = app;
--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,8 @@
 const notes = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
-notes.get('/', (req,res)=>
-);
+notes.get('/', (req, res) =>{
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+module.exports = notes;
